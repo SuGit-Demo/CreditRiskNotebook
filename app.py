49aba9c2072ed8ac55c5887e429f5e0a3d78fc23
SuGit-Demo <89434151+SuGit-Demo@users.noreply.github.com>
2022-07-01 20:38:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,75 +11,22 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    LoanDuration = request.form.get('duration')
+    Gender = request.form.get('gender')
+    if(Gender=='Female'):
+        Gender=0
+    else:
+        Gender=1
+    MStatus = request.form.get('mstatus')
+    if(MStatus=='Single'):
+        MStatus=0
+    else:
+        MStatus=1
+    Appincome = request.form.get('appincome')
+    Coincome = request.form.get('coincome')
     LoanAmount = request.form.get('amount')
-    Age = request.form.get('age')
-        
-    #output = round(prediction[0], 2)    
+    LoanDuration = request.form.get('duration')              
    
-    ####################### FROM AUTOAI DEPLOYMENT API #######################
-    # NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
-    API_KEY = "JFEp-T-YU4Wsdf5bxxTILX1avq0HTCdO0t4P_EMt-RNm"
-    token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
-     API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
-    mltoken = token_response.json()["access_token"]
-
-    header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
-
-    #LoanDuration = input('Enter Loan Duration')
-    #LoanAmount = input('Enter Loan Amount')
-    #Age = input('Enter Age')
-
-    # NOTE: manually define and pass the array(s) of values to be scored in the next line
-    payload_scoring = {"input_data": [{"fields": [
-                    "CheckingStatus",
-                    "LoanDuration",
-                    "CreditHistory",
-                    "LoanPurpose",
-                    "LoanAmount",
-                    "ExistingSavings",
-                    "EmploymentDuration",
-                    "InstallmentPercent",
-                    "Sex",
-                    "OthersOnLoan",
-                    "CurrentResidenceDuration",
-                    "OwnsProperty",
-                    "Age",
-                    "InstallmentPlans",
-                    "Housing",
-                    "ExistingCreditsCount",
-                    "Job",
-                    "Dependents",
-                    "Telephone",
-                    "ForeignWorker"],
-        "values": [[
-
-                    None,
-                    LoanDuration,
-                    None,
-                    None,
-                    LoanAmount,
-                    None,
-                    None,
-                    None,
-                    None,
-                    None,
-                    None,
-                    None,
-                    Age,
-                    None,
-                    None,
-                    None,
-                    None,
-                    None,
-                    None,
-                    None ]]
-        }]}
-
-    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/7a871bb8-b73b-4099-b4b5-34ff8a331883/predictions?version=2022-06-30', json=payload_scoring,
-     headers={'Authorization': 'Bearer ' + mltoken})
-    print("Scoring response")
-    print(response_scoring.json())
+    
     ####################### END OF AUTOAI DEPLOYMENT API #######################
 
     return render_template('index.html', prediction_text='Loan Risk Prediction is $ {}'.format(response_scoring.json()))
