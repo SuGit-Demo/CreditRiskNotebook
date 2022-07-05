a745e9f61b2b112114a22c3bd3644293c3a2ea77
SuGit-Demo <89434151+SuGit-Demo@users.noreply.github.com>
2022-07-05 14:08:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+from flask_db2 import DB2 #for DB2 connection
 import numpy as np
 import pickle
 import requests
@@ -15,7 +16,46 @@ def home():
 def login():
     username = request.form.get('username')
     password = request.form.get('password')
+    
+    ############################
+    dsn_hostname = "98538591-7217-4024-b027-8baa776ffad1.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud"# e.g.: "dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net"
+    dsn_uid = "vjd81886"# e.g. "abc12345"
+    dsn_pwd = "OicRrtEgVpnkIaxU"# e.g. "7dBZ3wWt9XN6$o0J"
+    
+    dsn_driver = "{IBM DB2 ODBC DRIVER}"
+    dsn_database = "bludb"            # e.g. "BLUDB"
+    dsn_port = "30875"                # e.g. "50000" 
+    dsn_protocol = "TCPIP"            # i.e. "TCPIP"
+    dsn_security = "SSL"
+    
+    dsn = (
+    "DRIVER={0};"
+    "DATABASE={1};"
+    "HOSTNAME={2};"
+    "PORT={3};"
+    "PROTOCOL={4};"
+    "UID={5};"
+    "PWD={6};"
+    "SECURITY={7};").format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd, dsn_security)
+    
+    try:
+        conn = ibm_db.connect(dsn, "", "")
+        print ("Connected to database: ", dsn_database, "as user: ", dsn_uid, "on host: ", dsn_hostname)        
+    except:
+        print ("no connection:", ibm_db.conn_errormsg())
+
+    #query_str = f"select * from VJD81886.CUSTOMER where COLUMN_0 = '{input_fruit}'"
+    query_str = f"select * from VJD81886.CUSTOMER where Username = '{username}' and Password = '{password}'"
+    selectQuery = query_str
+    
+    #Execute the statement
+    selectStmt = ibm_db.exec_immediate(conn, selectQuery)
+    
+    #Fetch the Dictionary (for the first row only) - replace ... with your code
+    output = ibm_db.fetch_tuple(selectStmt)    
+    
     isTrue = login.checkLogin(username,password)    
+    
     if(isTrue==1):
         return render_template('index.html')
     else:
